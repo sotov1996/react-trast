816c6e4a0b0b8ef5f5e3452925bc828b1f1d7ff3
ILYA <ilyapoliana@gmail.com>
2021-03-05 16:56:04 +0300

    54

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const bodyParser = require("body-parser");
 const cors = require("cors")
+const path = require("path")
 
 dotenv.config({ path: ".env" })
 
