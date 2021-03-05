4eb000063169ef44e2c56c894239f0532323dbeb
ILYA <ilyapoliana@gmail.com>
2021-03-05 20:56:11 +0300

    end

--- index.js
+++ index.js
@@ -7,21 +7,26 @@ const path = require("path")
 
 dotenv.config({ path: ".env" })
 
+const port = process.env.PORT || 4000
+const DB_HOST = process.env.DB_HOST
+
 mongoose.connect("mongodb+srv://trast_app:8scIdjQCb5nhoexd@cluster0.zuy1s.mongodb.net/trast", {
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
 })
-
-const port = process.env.PORT || 4000
-const DB_HOST = process.env.DB_HOST
 const app = express();
-app.use(express.static("/../built"));
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors())
-app.use("/api", require("./routes/product"))
+app.use("/api", require("./server/routes/product"))
+
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/build/index.html'));
+});
 
 /*mongoose.connect(DB_HOST,
     {
